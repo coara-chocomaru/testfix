677c9ed6aa60fae05d8ca68b4d06316a0515cbe2
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2025-10-16 02:20:24 +0900

    Add shared library and executable for gfxshim

--- Android.mk
+++ Android.mk
@@ -7,3 +7,24 @@ LOCAL_LDLIBS := -llog -ldl
 LOCAL_CPPFLAGS := -fPIC -std=c++11 -D_GNU_SOURCE -pthread
 include $(BUILD_SHARED_LIBRARY)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := libgfxshim
+LOCAL_SRC_FILES := libgfxshim.cpp
+# build as C++ shared lib
+LOCAL_CPPFLAGS := -D_GNU_SOURCE -fPIC -std=gnu++11
+LOCAL_LDLIBS := -ldl -llog -lpthread
+LOCAL_MODULE_TAGS := optional
+# Build for both 32/64
+LOCAL_MULTILIB := both
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := gfxshim_loader
+LOCAL_SRC_FILES := gfxshim_loader.c
+LOCAL_CFLAGS := -D_GNU_SOURCE
+LOCAL_LDLIBS := -ldl -llog -lpthread
+LOCAL_MODULE_TAGS := optional
+LOCAL_MULTILIB := both
+include $(BUILD_EXECUTABLE)
+
+
