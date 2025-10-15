40f233ed9633815aec77cbb0c1108877e635ca88
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2025-10-15 20:30:05 +0900

    Add files via upload

---
+++ shim.h
@@ -0,0 +1,16 @@
+
+#pragma once
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void _ZN7android13GraphicBufferC1Ejjij(void* thiz, unsigned int w, unsigned int h, int format, unsigned int usage);
+void _ZN7android13GraphicBufferC2Ejjij(void* thiz, unsigned int w, unsigned int h, int format, unsigned int usage);
+void _ZN7android13GraphicBufferD1Ev(void* thiz);
+void _ZN7android13GraphicBufferD2Ev(void* thiz);
+
+#ifdef __cplusplus
+}
+#endif
