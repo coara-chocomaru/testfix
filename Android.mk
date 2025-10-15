48f5a8da62508a041aff6a666c2b1fab3bd9f58f
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2025-10-15 21:37:35 +0900

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -3,6 +3,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := libgfxshim
 LOCAL_SRC_FILES := shim.cpp
 LOCAL_C_INCLUDES := $(LOCAL_PATH)
-LOCAL_LDLIBS := -llog -ldl -lpthread
-LOCAL_CPPFLAGS := -fPIC -std=c++11 -D_GNU_SOURCE
+LOCAL_LDLIBS := -llog -ldl
+LOCAL_CPPFLAGS := -fPIC -std=c++11 -D_GNU_SOURCE -pthread
 include $(BUILD_SHARED_LIBRARY)
