2cac01e89f75454ee7b2f737066f8216dcea3eaa
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2025-10-16 02:26:11 +0900

    Remove libgfxshim build configuration

--- Android.mk
+++ Android.mk
@@ -8,15 +8,6 @@ LOCAL_CPPFLAGS := -fPIC -std=c++11 -D_GNU_SOURCE -pthread
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := libgfxshim
-LOCAL_SRC_FILES := libgfxshim.cpp
-LOCAL_CPPFLAGS := -D_GNU_SOURCE -fPIC -std=gnu++11
-LOCAL_LDLIBS := -ldl -llog -lpthread
-LOCAL_MODULE_TAGS := optional
-LOCAL_MULTILIB := both
-include $(BUILD_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE := gfxshim_loader
 LOCAL_SRC_FILES := gfxshim_loader.c
 LOCAL_CFLAGS := -D_GNU_SOURCE
@@ -24,5 +15,3 @@ LOCAL_LDLIBS := -ldl -llog -lpthread
 LOCAL_MODULE_TAGS := optional
 LOCAL_MULTILIB := both
 include $(BUILD_EXECUTABLE)
-
-
