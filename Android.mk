a9defc95fbfbf2abb7d0eb6c91c73105af312bcc
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2025-10-15 20:29:33 +0900

    Create Android.mk

---
+++ Android.mk
@@ -0,0 +1,8 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_MODULE := libgfxshim
+LOCAL_SRC_FILES := shim.cpp
+LOCAL_C_INCLUDES := $(LOCAL_PATH)
+LOCAL_LDLIBS := -llog -ldl -lpthread
+LOCAL_CPPFLAGS := -fPIC -std=c++11 -D_GNU_SOURCE
+include $(BUILD_SHARED_LIBRARY)
