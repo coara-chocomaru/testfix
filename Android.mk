ff9c8e581fa0d75f9564d1f07a6299696a135d94
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2025-10-17 03:26:21 +0900

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -32,8 +32,8 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := libshim3
 LOCAL_SRC_FILES := shim3.cpp
 LOCAL_CFLAGS := -fPIC -DANDROID -O2
-LOCAL_CPPFLAGS := -fPIC -std=gnu++11 -DANDROID -O2 -Wall -Wno-unused-parameter -Wno-missing-field-initializers -std=c++17
-LOCAL_LDLIBS := -llog -ldl
+LOCAL_CPPFLAGS := -fPIC -std=gnu++11 -DANDROID -O2 -Wall -Wno-unused-parameter -Wno-missing-field-initializers -std=c++11
+LOCAL_LDLIBS := -llog -ldl -lc++
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 include $(BUILD_SHARED_LIBRARY)
