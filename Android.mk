618fc99183ca29487ae7fa61a099007f32c552d7
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2025-10-17 04:14:25 +0900

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -33,8 +33,6 @@ LOCAL_MODULE := libshim3
 LOCAL_SRC_FILES := shim3.cpp
 LOCAL_CPP_FEATURES := exceptions rtti
 LOCAL_CPPFLAGS += -std=c++17 -fexceptions -frtti -stdlib=libc++
-LOCAL_STATIC_LIBRARIES += c++_static
-LOCAL_LDFLAGS += -stdlib=libc++ -Wl,--whole-archive
 LOCAL_LDLIBS := -llog -ldl
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
