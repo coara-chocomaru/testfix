a60957a76f5c9aa9697dec5b89a05299062cfac9
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2025-10-17 03:19:25 +0900

    Update Application.mk

--- Application.mk
+++ Application.mk
@@ -1,7 +1,7 @@
 APP_ABI := armeabi-v7a arm64-v8a
 APP_PLATFORM := android-21
 APP_STL := c++_static
-APP_CPPFLAGS := -fPIC -std=gnu++17 -std=c++11  -D_GNU_SOURCE -fexceptions -frtti -O2
+APP_CPPFLAGS := -fPIC -std=gnu++17 -std=c++11 -D_GNU_SOURCE -fexceptions -frtti -O2
 APP_LDFLAGS := -Wl,--exclude-libs,libgcc.a
 ifeq ($(TARGET_ARCH_ABI), arm64-v8a)
 APP_CPPFLAGS += -D__aarch64__ 
