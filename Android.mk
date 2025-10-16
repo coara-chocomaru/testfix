5a0223b95a15f2b6e92782a87b1d51428cbe17dd
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2025-10-16 23:09:20 +0900

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -20,14 +20,3 @@ LOCAL_CPPFLAGS += -fPIC -std=c++11 -D_GNU_SOURCE -Wall -Wno-write-strings
 LOCAL_LDLIBS := -llog -ldl
 LOCAL_ALLOW_MULTIPLE_DEFINES := true
 include $(BUILD_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := libui_shim
-LOCAL_MODULE_TAGS := optional
-LOCAL_SRC_FILES := test.c
-LOCAL_SHARED_LIBRARIES := libui liblog libbase libc++
-LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)
-LOCAL_CFLAGS := -O3 -Wno-unused-variable -Wno-unused-parameter
-LOCAL_PROPRIETARY_MODULE := true
-LOCAL_32_BIT_ONLY := false
-include $(BUILD_SHARED_LIBRARY)
