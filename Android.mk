adc1d53ffce0796966e808392fcb3681948a88f4
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2025-10-18 13:47:41 +0900

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -27,19 +27,3 @@ LOCAL_SRC_FILES := shim2.cpp
 LOCAL_CPPFLAGS := -fPIC -std=gnu++11 -DANDROID -O2 -Wall -Wno-unused-parameter -Wno-missing-field-initializers
 LOCAL_LDLIBS := -llog -ldl
 include $(BUILD_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := fix_data_perm
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := EXECUTABLES
-LOCAL_MODULE_PATH := $(TARGET_OUT)/bin
-LOCAL_SRC_FILES := fixdata.c
-LOCAL_CFLAGS += -Wall -Werror -Wno-unused-parameter
-LOCAL_FORCE_STATIC_EXECUTABLE := true
-LOCAL_STATIC_LIBRARIES := \
-    libc \
-    libcutils
-ifeq ($(TARGET_ARCH),arm64)
-    LOCAL_CFLAGS += -DARCH_ARM64
-endif
-include $(BUILD_EXECUTABLE)
